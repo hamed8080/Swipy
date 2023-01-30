adbc98f3453ea1dbca45ea977f4163e29d78f305
Hamed Hosseini <hamed8080@gmail.com>
2023-01-30 13:03:35 +0330

    - First commit

--- README.md
+++ README.md
@@ -1,3 +1,45 @@
 # Swipy
+<br />
+<br />
 
-A description of this package.
+### Do you want Widget-style animation on iOS devices in your app by swiping and changing the view?
+<br />
+
+## Installation
+
+#### SPM 
+
+Add in `Package.swift`:
+
+```swift
+.package(url: "https://github.com/hamed8080/Swipy", branch: "main")
+```
+
+## Usage 
+```swift
+Swipy(users, containerHeight: containerHeight) { user in
+    HStack {
+        Text("Test")
+            .font(.subheadline)
+    }
+    .frame(minHeight: 0, maxHeight: containerHeight)
+    .background(.green.opacity(0.5))
+    .cornerRadius(24)
+} onSwipe: { item in
+    selectedItem = item
+}
+.frame(minHeight: 0, maxHeight: containerHeight)
+.background(.ultraThinMaterial)
+.cornerRadius(24)
+```
+<br/>
+<br/>
+
+
+## Developer Application 
+Check out `SwipyPreview.swift` for an in-action example.
+
+
+## Contributing to Swipy
+
+Please see the [contributing guide](/CONTRIBUTING.md) for more information.
--- Sources/Swipy/Swipy.swift
+++ Sources/Swipy/Swipy.swift
@@ -1,6 +1,159 @@
-public struct Swipy {
-    public private(set) var text = "Hello, World!"
+//
+//  ContentView.swift
+//  SwiftUISamples
+//
+//  Created by hamed on 1/29/23.
+//
 
-    public init() {
+import SwiftUI
+
+struct Swipy<T: Identifiable, ItemView: View>: View, SwipyProtocol {
+    typealias Item = T
+
+    public var containerHeight: CGFloat = 100
+    public var containerWidth: CGFloat = 346
+    public var containerCornerRadius: CGFloat = 24
+    public var edgeBouncerAmount: CGFloat = 10
+    public var onTouchDownScaleX: CGFloat = 0.95
+    public var maxScaleDownX: CGFloat = 0.85
+    public var maxScaleDownY: CGFloat = 0.85
+    public var animation: Animation = .interactiveSpring(response: 0.1, dampingFraction: 0.5).speed(0.2)
+
+    @State private var offsetY: CGFloat = .zero
+    @State private var scaleY: CGFloat = 1
+    @State private var scaleX: CGFloat = 1
+    @State private var isDragging = false
+    @State private var draggingIndex: Int = 0
+    @State private var dragValue: DragGesture.Value?
+
+
+    private var isTranslationGreaterThanOneFourth: Bool { abs(trHeight) > (containerHeight / 8) }
+    private var trHeight: CGFloat { dragValue?.translation.height ?? 0 }
+    private var trWidth: CGFloat { dragValue?.translation.width ?? 0 }
+    private var isTopIndex: Bool { draggingIndex == 0 }
+    private var startIndex: Int { items.startIndex }
+    private var lastIndex: Int { items.endIndex - 1 }
+    private var isLastIndex: Bool { draggingIndex == lastIndex }
+    private var isGreaterThanLastIndex: Bool { draggingIndex > lastIndex }
+    private var isDraggingDown: Bool { dragValue?.translation.height ?? 0 > 0 }
+    private var isDraggingUp: Bool { dragValue?.translation.height ?? 0 < 0 }
+    private var maxAllowedDraggUp: CGFloat { -((lastIndex.cgFloatValue * containerHeight) + edgeBouncerAmount) }
+    private var currentTranslationIsBiggerThanMinAllowed: Bool { isDraggingDown && (offsetY + trHeight) > 0 }
+    private var currentTranslationIsBiggerThanMaxAllowed: Bool { abs(offsetY + trHeight) > abs(maxAllowedDraggUp) }
+    private var isOverMin: Bool { currentTranslationIsBiggerThanMinAllowed || (isDraggingDown && isTopIndex) }
+    private var isOverMax: Bool { isDraggingUp && (draggingIndex >= lastIndex || currentTranslationIsBiggerThanMaxAllowed) }
+
+
+    typealias OnItemView = (Item) -> (ItemView)
+    typealias OnSwipe = (Item) -> ()
+    var onItemView: OnItemView
+    var onSwipe: OnSwipe?
+
+    var items: [Item]
+
+    init(
+        _ items: [Item],
+        containerHeight: CGFloat = 100,
+        containerWidth: CGFloat = 346,
+        containerCornerRadius: CGFloat = 24,
+        edgeBouncerAmount: CGFloat = 10,
+        onTouchDownScaleX: CGFloat = 0.95,
+        maxScaleDownX: CGFloat = 0.85,
+        maxScaleDownY: CGFloat = 0.85,
+        animation: Animation = .interactiveSpring(response: 0.1, dampingFraction: 0.5).speed(0.2),
+        @ViewBuilder _ onItemView: @escaping OnItemView,
+        onSwipe: @escaping OnSwipe
+    ) {
+        self.items = items
+        self.containerHeight = containerHeight
+        self.containerWidth = containerWidth
+        self.containerCornerRadius = containerCornerRadius
+        self.edgeBouncerAmount = edgeBouncerAmount
+        self.onTouchDownScaleX = onTouchDownScaleX
+        self.maxScaleDownX = maxScaleDownX
+        self.maxScaleDownY = maxScaleDownY
+        self.animation = animation
+        self.onItemView = onItemView
+        self.onSwipe = onSwipe
+    }
+
+
+    var drag: some Gesture {
+        DragGesture(minimumDistance: 10)
+            .onChanged { value in
+                withAnimation(animation) {
+                    dragValue = value
+                    if isOverMin {
+                        draggingIndex = startIndex
+                        offsetY = edgeBouncerAmount
+                        return
+                    }
+
+                    if isOverMax {
+                        draggingIndex = lastIndex
+                        offsetY = maxAllowedDraggUp
+                        return
+                    }
+                    offsetY += trHeight
+                    scaleY = abs(max(maxScaleDownY, (trHeight / 100) - 1))
+                    scaleX = abs(max(maxScaleDownX, (trWidth / 100) - 1))
+                }
+            }
+            .onEnded { _ in
+                withAnimation(animation) {
+                    isDragging = false
+                    if isTranslationGreaterThanOneFourth {
+                        if isDraggingDown {
+                            draggingIndex = isTopIndex ? startIndex : draggingIndex - 1
+                        } else if isDraggingUp {
+                            draggingIndex = isLastIndex ? lastIndex : draggingIndex + 1
+                        }
+                    }
+                    offsetY = -(draggingIndex.cgFloatValue * containerHeight)
+                    onSwipe?(items[draggingIndex])
+                }
+            }
+    }
+
+    var longGesture: some Gesture {
+        LongPressGesture(minimumDuration: 0.002)
+            .onEnded { _ in
+                withAnimation {
+                    scaleY = onTouchDownScaleX
+                    scaleX = onTouchDownScaleX
+                    isDragging = true
+                }
+            }
+    }
+
+    var combinedGesture: some Gesture {
+        SequenceGesture(longGesture, drag)
+    }
+
+    var body: some View {
+        ScrollView {
+            VStack(spacing: 0) {
+                ForEach(items) { item in
+                    HStack {
+                        onItemView(item)
+                    }
+                    .frame(width: containerWidth, height: containerHeight)
+                    .cornerRadius(containerCornerRadius)
+                    .scaleEffect(x: isDragging ? scaleX : 1, y: isDragging ? scaleY : 1, anchor: .center)
+                    .offset(CGSize(width: 0, height: offsetY))
+                    .gesture(combinedGesture)
+                    .onLongPressGesture(minimumDuration: 0.002, maximumDistance: 0) {} onPressingChanged: { isPressing in
+                        withAnimation {
+                            scaleY = isPressing ? onTouchDownScaleX : 1
+                            scaleX = isPressing ? onTouchDownScaleX : 1
+                            isDragging = isPressing
+                        }
+                    }
+                }
+            }
+        }
+        .frame(width: containerWidth, height: containerHeight, alignment: .top)
+        .cornerRadius(containerCornerRadius)
     }
 }
+
